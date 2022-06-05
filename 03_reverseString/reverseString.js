bcf2ebd361d793d664d7585fe91b3496c7bad0ad
divijkhaitan <divijkhaitan@gmail.com>
2022-06-05 22:00:46 +0530

    Solve Q3 and remove test skips

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputstring) {
+    const array=inputstring.split("");
+    let result=""
+    for (i=array.length-1;i>=0;i--)
+    {
+        result+=array[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
