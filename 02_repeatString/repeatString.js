0ec82ceab1e307461eda1651ddb251b6c7b5080f
divijkhaitan <divijkhaitan@gmail.com>
2022-06-05 21:53:16 +0530

    Solve Q2 and remove skip from tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(inputstring, number) {
+    let result=""
+    if(number<0)
+    return "ERROR"
+    for (let i=0;i<number;i++)
+    {
+        result+=inputstring
+    }
+    return result
+}
 
 // Do not edit below this line
 module.exports = repeatString;
