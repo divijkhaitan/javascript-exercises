97a0ad5b109b4981b019c9dd6aba09491f9ee664
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 13:01:42 +0530

    Solve Q5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,l) {
+    if(typeof(a)!="number")
+    return "ERROR"
+    if(typeof(l)!="number")
+    return "ERROR"
+    if(a<0||l<0)
+    {
+        return "ERROR"
+    }
+    let numbers=Math.max(a,l)-Math.min(a,l)
+    let total=(a+l)*(numbers+1)/2
+    return total
 };
 
 // Do not edit below this line
