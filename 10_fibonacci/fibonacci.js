1c8552731da8f6a0a3b8048dba8922560ac11575
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 15:09:53 +0530

    Solve Q10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num=parseInt(num)
+    if(num<0)
+    {
+        return "OOPS"
+    }
+    let fibn1=1
+    let fibn2=1
+    let temp=0
+    for (let i=2;i<num;i++)
+    {
+        temp=fibn1+fibn2
+        fibn1=fibn2
+        fibn2=temp
+    }
+    return fibn2
 };
 
 // Do not edit below this line
