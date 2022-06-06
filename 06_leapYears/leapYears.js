e7f0866727d1245064b37b0c7b9e0418ce01fd11
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 13:07:33 +0530

    Solve Q6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    let leap=false
+    if(typeof(years)!="number")
+    return "ERROR"
+    if(years<0)
+    return "ERROR"
+    if (years%4==0)
+    {
+        leap=true
+    }
+    if (years%100==0)
+    {
+        leap=false
+    }
+    if (years%400==0)
+    {
+        leap=true
+    }
+    return leap
 };
 
 // Do not edit below this line
