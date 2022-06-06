48911a42a356408821b55261a6dc069c1c47d87e
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 14:28:56 +0530

    Solve Q8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let count=0
+  for (let i=0;i<arr.length;i++)
+  {
+    count+=arr[i]
+  }
+  return count;
 };
 
-const multiply = function() {
-
+const multiply = function(mults) {
+  let prod=1
+  for (let i=0;i<mults.length;i++)
+  {
+    prod*=mults[i]
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact=1
+  for (let i=num;i>0;i--)
+  {
+    fact*=i
+  }
+  return fact;
 };
 
 // Do not edit below this line
