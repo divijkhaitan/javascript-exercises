6cc360cd898688c3216164d853060bbea933084c
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 13:23:53 +0530

    Solve Q7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if(typeof temp!="number")
+  return "ERROR"
+  temp=(temp-32)*5/9
+  temp=Math.round(temp*10)/10
+  return temp
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if(typeof temp!="number")
+  return "ERROR"
+  temp=(temp*9)/5 +32
+  temp=Math.round(temp*10)/10
+  return temp
 };
 
 // Do not edit below this line
