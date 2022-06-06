37195625a51cbb45ec64686467008929421afea4
divijkhaitan <divijkhaitan@gmail.com>
2022-06-06 12:52:34 +0530

    Solve Q4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputarr,...rm) {
+    let removed=false
+    let count=0
+    let partial=[]
+    for (; inputarr.length>0;)
+    {
+        if(rm.includes(inputarr[inputarr.length-1]))
+        {
+            inputarr.pop();
+        }
+        else
+        {
+            partial.push(inputarr.pop())
+        }
 
+    }
+    return partial.reverse()
 };
 
 // Do not edit below this line
