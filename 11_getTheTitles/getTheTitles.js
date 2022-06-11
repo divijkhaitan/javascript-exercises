ac699ac602cf8ddfa2d1e96c3ebf4ba36dbb27fe
divijkhaitan <divijkhaitan@gmail.com>
2022-06-11 15:02:20 +0530

    Solve Q11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const arr=[]
+    books.forEach(book=>arr.push(book.title))
+    return arr
 };
 
 // Do not edit below this line
