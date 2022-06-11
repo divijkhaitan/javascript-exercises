a1497817b7b2636b0c1e6316cff0b2be0ac5f6ca
divijkhaitan <divijkhaitan@gmail.com>
2022-06-11 15:23:10 +0530

    Solve Q12, working in other environments but not passing tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const arr1=arr.map(person=>(person.yearOfDeath||2022)-(person.yearOfBirth))
+    let max=arr1[0]
+    let oldest=arr[0].name
+    for (let i=0;i<arr1.length;i++)
+    {
+        if(arr1[i]>max)
+        {
+            max=arr1[i]
+            oldest=arr[i].name
+        }
+    }
+    return oldest
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
